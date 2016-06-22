9ac2b82a6559c2d37121cce107003a1479022859
aashraya <aashraya_p@hotmail.com>
2016-06-22 16:01:16 -0500

    added winning diagonal initial conditions

--- board.rb
+++ board.rb
@@ -59,13 +59,15 @@ class Board
        end
    end
    def winning_combination?
-       winning_vertical? || winning_horizontal?
+       winning_vertical? || winning_horizontal? ||winning_diagonal?
        #winning_diagonal? || winning_vertical? || winning horizontal?
    end
    def winning_vertical?
        # coordinate=[@column,@rowcoordinate]
        # verticalarray=[@board[@column.to_i][@rowcoordinate.to_i],@board[@column.to_i][@rowcoordinate.to_i-1],@board[@column.to_i][@rowcoordinate.to_i-2],@board[@column.to_i][@rowcoordinate.to_i-3]]
-       verticalarray=[@board[@column.to_i][6],@board[@column.to_i][5],@board[@column.to_i][4],@board[@column.to_i][3],@board[@column.to_i][2],@board[@column.to_i][1],@board[@column.to_i][0]]
+       verticalarray=[@board[@column.to_i][6],@board[@column.to_i][5],
+       @board[@column.to_i][4],@board[@column.to_i][3],@board[@column.to_i][2],
+       @board[@column.to_i][1],@board[@column.to_i][0]]
        four_in_a_row?(verticalarray)
    end
    def four_in_a_row?(arr)
@@ -75,6 +77,25 @@ class Board
    		horizontalarray=[@board[0][@rowcoordinate],@board[1][@rowcoordinate],@board[2][@rowcoordinate],@board[3][@rowcoordinate],@board[4][@rowcoordinate],@board[5][@rowcoordinate]]
    		four_in_a_row?(horizontalarray)
    end
+
+   def winning_diagonal?
+   		#red is one diagonal, green is another
+   		#red
+   		d1= [@board[2][0],@board[3][1],@board[4][2],@board[5][3]]
+   		d2= [@board[1][0],@board[2][1],@board[3][2],@board[4][3],@board[5][4]
+   		d3= [@board[0][0],@board[1][1],@board[2][2],@board[3][3],@board[4][4],@board[5][5]	
+		d4= [@board[0][1],@board[1][2],@board[2][3],@board[3][4],@board[4][5],@board[5][6]]
+   		d5=  [@board[0][2],@board[1][3],@board[2][4],@board[3][5],@board[4][6]]
+   		d6= [@board[0][3],@board[1][4],@board[2][5],@board[3][6]]
+   		#green
+   		d7=[@board][3][0],@board][2][1],@board][1][2],@board][0][3]] 
+   		d8=[@board][4][0],@board][3][1],@board][2][2],@board][1][3],@board][0][4]]
+   		d9=[@board][5][0],@board][4][1],@board][3][2],@board][2][3],@board][1][4],
+        @board][0][5]] 
+        d10 = [@board[5][1], @board[4][2], @board[3][3], @board[2][4], @board[1][5], @board[0][6]]
+   		d11 = [@board[5][2], @board[4][3], @board[3][4], @board[2][5], @board[1][6]]
+       	d12 = [@board[5][3], @board[4][4], @board[3][5], @board[2][6]]
+  	end
 end
 
 
